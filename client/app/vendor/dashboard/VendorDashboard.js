6c44016bed3b5117c251fc52f205a66da17acf2c
suthakaranburaj <suthakaranburaj@gmail.com>
2025-07-27 00:49:56 +0530

    12:49pm

--- client/app/vendor/dashboard/VendorDashboard.js
+++ client/app/vendor/dashboard/VendorDashboard.js
@@ -44,7 +44,7 @@ export default function VendorDashboard({ user, vendorData }) {
   
   return (
     <div className="min-h-screen bg-gray-50 dark:bg-gray-900">
-      <Navbar />
+      {/* <Navbar /> */}
       
       <main className="py-8">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
