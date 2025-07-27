1a2f94026b992aa099f98e02c66e9172b04dc685
suthakaranburaj <suthakaranburaj@gmail.com>
2025-07-27 17:47:13 +0530

    small url change

--- server/src/app.js
+++ server/src/app.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(cors({
     origin: [
         "http://localhost:3000",
-        "https://tutedude-2025.vercel.app/"
+        "https://tutedude-2025.vercel.app"
     ],
     credentials: true
 }));
